5b0569767dfab4d466a95aa39725e9fc0630ebd2
hminaya <hminaya@gmail.com>
2014-02-08 18:20:13 -0400

    funciona

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,6 @@
 angular.module("microcarros",[])
 .controller("principal",function main($scope,carros){
 
-
-	
-
-
-
-
 	// Yo traigo datos
 	// El intermediario entre el VIEW y el MODEL
 	// Scope: Soy un canal para pasar el modelo al view??
--- public/js/controllers/index.js
+++ public/js/controllers/index.js
@@ -1,8 +1,17 @@
 angular.module("microcarros")
 .controller("index",function main($scope,$http){
 
-	//console.log("Llego la Smirnoff ICE...")
+	//$http.get("http://localhost:3001/js/vehicle.json")
+	$http.get("http://192.168.1.43:3000/api/vehicle")
+	.then(function(response){
 
-	
+		console.log(response);
+		$scope.carros = response.data.result;
+
+	},function(error){
+
+		console.log(error);
+
+	})
 
 });
